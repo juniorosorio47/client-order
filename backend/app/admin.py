230bb655f6cade2bfad1e253a0cd8b2bae2e6e0f
Junior Osorio <juniorosorio47@gmail.com>
2021-10-13 07:05:43 +0800

    Initial commit

---
+++ backend/app/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Client, Product, Order
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name','id' )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock_amount')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'products', 'amount')
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
